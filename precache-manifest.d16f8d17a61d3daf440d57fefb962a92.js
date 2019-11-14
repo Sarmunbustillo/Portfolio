6e7cf4fee120495ec822ef6d2e26abafa4ec06d1
Sarmunbustillo <sarchamo26@gmail.com>
2019-11-14 22:23:01 +0100

    Updates

---
+++ precache-manifest.d16f8d17a61d3daf440d57fefb962a92.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec15d39a09597ecb4145b4dc66092442",
+    "url": "/porfolio/index.html"
+  },
+  {
+    "revision": "373ab91ebd785a738215",
+    "url": "/porfolio/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "342f4ea59d59ee9ba82d",
+    "url": "/porfolio/static/css/main.63d1dd2c.chunk.css"
+  },
+  {
+    "revision": "373ab91ebd785a738215",
+    "url": "/porfolio/static/js/2.f2275e89.chunk.js"
+  },
+  {
+    "revision": "342f4ea59d59ee9ba82d",
+    "url": "/porfolio/static/js/main.e700a2b2.chunk.js"
+  },
+  {
+    "revision": "80cb91e9e52096376b00",
+    "url": "/porfolio/static/js/runtime-main.05258e58.js"
+  },
+  {
+    "revision": "06964aedc8d27ed376bf6b5c28861584",
+    "url": "/porfolio/static/media/budget-calc.06964aed.png"
+  },
+  {
+    "revision": "c1d71d8e442a29773a066a08704bba9d",
+    "url": "/porfolio/static/media/cookIt.c1d71d8e.png"
+  },
+  {
+    "revision": "9604c5a6dfe6102cceb75d8f15674843",
+    "url": "/porfolio/static/media/window.9604c5a6.JPG"
+  }
+]);
