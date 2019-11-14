4546aea12555c105fb037aa0c75a404d549c6200
Sarmunbustillo <sarchamo26@gmail.com>
2019-11-14 19:31:07 +0100

    moved filed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './index.css';
+import App from './src/App';
 import * as serviceWorker from './serviceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
